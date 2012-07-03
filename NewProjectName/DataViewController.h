511694ba22f593aa72d3034837d19a0619b02c96
Gaurav Mantrao <gmantrao@live.com>
2012-07-03 14:52:35 +0530

    Initial Commit

---
+++ NewProjectName/DataViewController.h
@@ -0,0 +1,14 @@
+//
+//  DataViewController.h
+//  NewProjectName
+//
+//  Created by Gaurav Mantrao on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+@end
---
+++ NewProjectName/ModelController.h
@@ -0,0 +1,16 @@
+//
+//  ModelController.h
+//  NewProjectName
+//
+//  Created by Gaurav Mantrao on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class DataViewController;
+
+@interface ModelController : NSObject <UIPageViewControllerDataSource>
+- (DataViewController *)viewControllerAtIndex:(NSUInteger)index storyboard:(UIStoryboard *)storyboard;
+- (NSUInteger)indexOfViewController:(DataViewController *)viewController;
+@end
