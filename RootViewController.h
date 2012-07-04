61d1920087999a1260bb901eae06b2eb34089572
GM <guarvamantrao@live.com>
2012-07-04 14:33:16 +0530

    Initial commit

---
+++ RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  NewProjectName
+//
+//  Created by Gaurav Mantrao on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
